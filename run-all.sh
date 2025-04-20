528c68691b71b433410cc7b09e59abe9c2835277
Hororohoruru <juanjesustorre@gmail.com>
2025-04-20 13:13:41 +0200

    Updated run-all to reflect correct script structure

--- run-all.sh
+++ run-all.sh
@@ -5,10 +5,13 @@ cd "$(dirname "$0")/setup"
 
 ./00-post-install.sh
 ./01-install-packages.sh
-./02-setup-ssh.sh
+./02-setup-git.sh
 ./03-clone-dotfiles.sh
 ./04-stow-dotfiles.sh
-./05-install-mamba.sh
+./05-install-tpm.sh
+./06-set-default-shell.sh
+./07-install-mamba.sh
+./08-set-font.sh
 
 # Now, disable 'exit on error'
 set +e
