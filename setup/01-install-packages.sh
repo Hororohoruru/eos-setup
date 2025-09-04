5bc610c60e7a22982169afa020895028507af541
Hororohoruru <juanjesustorre@gmail.com>
2025-09-04 11:54:38 +0200

    Added block to check and install Yay if needed

--- setup/01-install-packages.sh
+++ setup/01-install-packages.sh
@@ -6,6 +6,18 @@ PACKAGE_LIST="./package-list.txt"
 echo "[*] Updating system..."
 sudo pacman -Syu --noconfirm
 
+echo "[*] Checking that yay is installed..."
+if command -v yay >/dev/null 2>&1; then
+  echo "Yay is already installed, good!"
+else
+  echo "Installing Yay..."
+  sudo pacman -S --needed --noconfirm git base-devel  
+  git clone "https://aur.archlinux.org/yay.git" "$HOME/gitrepos/yay"
+  cd "$HOME/gitrepos/yay"
+  makepkg -si
+  cd -
+fi
+
 echo "[*] Installing packages from $PACKAGE_LIST..."
 while IFS= read -r pkg; do
   if yay -Q "$pkg" &> /dev/null; then
